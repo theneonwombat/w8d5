770d5f6439ef433ad007ffe856e1b2ebb1cc9b13
joshebrani <joshuaebrani@gmail.com>
2021-02-19 15:52:45 -0500

    Clock complete

--- callbacks.js
+++ callbacks.js
@@ -2,8 +2,8 @@ class Clock {
   constructor() {
     let date = new Date();// 1. Create a Date object.
     this.time = [date.getHours(), date.getMinutes(), date.getSeconds()];// 2. Store the hours, minutes, and seconds.
-    this.printTime(time)// 3. Call printTime.
-    this._tick(time)// 4. Schedule the tick at 1 second intervals.
+    this.printTime()// 3. Call printTime.
+    setInterval(this._tick.bind(this), 1000)// 4. Schedule the tick at 1 second intervals.
   }
 
   printTime() {
@@ -12,11 +12,30 @@ class Clock {
   }
 
   _tick() {
-    // 1. Increment the time by one second.
-    // 2. Call printTime.
+    // debugger
+   ++this.time[2] // 1. Increment the time by one second.
+   if (this.time[2] === 60) {
+     this.time[2] = 0;
+     this.time[1] += 1;
+   }
+    if (this.time[1] === 60) {
+      this.time[1] = 0;
+      this.time[0] += 1;
+    }
+
+    if (this.time[0] === 13) {
+      this.time[0] = 1
+    }
+    this.printTime()// 2. Call printTime.
   }
 }
 
+// console.log("taco")
+
 const clock = new Clock();
 
-[b.getHours(), b.getMinutes(), b.getSeconds()]
+// clock.printTime()
+
+// clock._tick()
+
+// clock.printTime()
